17740fd79da19baf70b63b73c53e204249cf2682
Gustavo Macedo <gustavo3vieira@gmail.com>
2024-02-20 15:59:53 -0300

    EX 38

---
+++ EX38(Comparando números)/index.py
@@ -0,0 +1,9 @@
+num_1 = int(input('Primeiro número : '))
+num_2 = int(input('Segundo número : '))
+if num_1 > num_2:
+    print('O PRIMEIRO número e maior')
+elif num_2 > num_1:
+    print('O SEGUNDO número e maior')
+else:
+    print('Os números são iguais !')
+    
