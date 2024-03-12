fe0fb3ff84e190e6ea1c02aecb9c014e6bdcbb4c
Gustavo Macedo <gustavo3vieira@gmail.com>
2024-03-12 14:57:02 -0300

    EX66

---
+++ EX66(Vários números com flag)/index.py
@@ -0,0 +1,13 @@
+contador = soma = 0
+
+while True:
+    numero = int(input('Digite um número, [999 PARA PARAR]: '))
+    
+    
+    if numero == 999:
+        break
+    
+    soma += numero 
+    contador += 1
+    
+print(f'Você digitou {contador} números e a soma entre eles e de {soma}')
